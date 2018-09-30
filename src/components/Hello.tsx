a9cbde46e1bb275f98a55e199d9bb522f8950ea7
silvia-YQY <545067444@qq.com>
2018-09-30 11:35:06 +0800

    尝试添加IState

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,22 +1,45 @@
 import * as React from 'react';
+import './Hello.css';
 
 export interface IProps {
-  name: string;
-  enthusiasmLevel?: number;
+	name: string;
+	enthusiasmLevel?: number;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: IProps) {
-  if (enthusiasmLevel <= 0) {
-    throw new Error('You could be a little more enthusiastic. :D');
-  }
+export interface IState {
+	date: string;
+}
 
-  return (
-    <div className="hello">
-      <div className="greeting">
-        Hello {name + getExclamationMarks(enthusiasmLevel)}
-      </div>
-    </div>
-  );
+class Hello extends React.Component<IProps, IState> {
+	public arr2 : string;
+	constructor(props: any) {
+		super(props)
+		this.state = {
+			date: 'ppp',
+		};
+		this.arr2 = '[2]' 
+		
+		// 譬如我承继了IProps，和IState，但是我还需要一个arr参数，
+		// 那是否还需要在外面定义接口的形式在承继才能用??
+	}
+	public render() {
+		const { name, enthusiasmLevel = 1 } = this.props;
+		const { date } = this.state;
+
+		console.log("state", this.state)  // 这里打印出来是有date：ppp 的。
+		if (enthusiasmLevel <= 0) {
+			throw new Error('You could be a little more enthusiastic. :D');
+		}
+
+		return (
+			<div className="hello">
+				<div className="greeting">
+					Hello {name + getExclamationMarks(enthusiasmLevel) + date + this.arr2}
+					{/* <h2>It is {date}.</h2> */}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Hello;
@@ -24,5 +47,5 @@ export default Hello;
 // helpers
 
 function getExclamationMarks(numChars: number) {
-  return Array(numChars + 1).join('!');
+	return Array(numChars + 1).join('!');
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,34 @@ import * as ReactDOM from 'react-dom';
 // import App from './App';
 import Hello from './components/Hello';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-	// <App />,
-	<Hello name="TypeScript" enthusiasmLevel={10} />,
-  document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+// const element = (
+// 	    <div>
+// 	      <h1>Hello, world!</h1>
+// 	      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+// 	    </div>
+// 	  );
+
+// ReactDOM.render(
+// 	// <App />,
+// 	element,
+// 	// <Hello name="TypeScript" enthusiasmLevel={10} />,
+//   document.getElementById('root') as HTMLElement
+// );
+
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+			<Hello name="TypeScript" enthusiasmLevel={10} />,
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root') as HTMLElement
+  );
+}
+
+setInterval(tick, 1000);
