7670a99a98c0cdf0e3f76fe6f7e288707001f63b
silvia-YQY <545067444@qq.com>
2018-10-08 17:09:45 +0800

    尝试添加Mount的三个钩子，componentWillMount,componentDidMount,render

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -8,32 +8,53 @@ export interface IProps {
 
 export interface IState {
 	date: Date;
+	count: number;
 }
 
 class Hello extends React.Component<IProps, IState> {
 	public arr2: string;
+	public num: number;
 	constructor(props: any) {
 		super(props)
 		this.state = {
 			date: new Date(),
+			count: 0,
 		};
 		this.arr2 = '[2]'
+		this.num = 1
+		console.log('我已经在 constructor 里将 props 和 state 初始化好了')
+
+
 
 		
-		setInterval(() => { // 搜索「JS 箭头函数 MDN」
-			this.setState({
-				date: new Date() // 更新 date
-			})
-		})
-
-		// 譬如我承继了IProps，和IState，但是我还需要一个arr参数，
-		// 那是否还需要在外面定义接口的形式在承继才能用??
+	}
+	public componentWillMount() {
+		// setInterval(() => { // 搜索「JS 箭头函数 MDN」
+		// 	this.incrementMultiple()
+		// }, 5000)
+		console.log('已经挂载到页面里了')
+	}
+	public componentDidMount(){
+		console.log('挂载完成了')
+	}
+	public increment(state: any, props: any) {
+		return { count: state.count + 1 };
+	}
+	public incrementMultiple() {
+		
+		this.setState({ count: this.state.count + 1 });
+		this.setState({ count: this.state.count + 1 });
+		this.setState({ count: this.state.count + 1 });
+		// this.setState(this.increment);
+		// this.setState(this.increment);
+		// this.setState(this.increment);
+		//this.setState({count: this.state.count + 1});
+		console.log("setState", this.state.count)
 	}
 	public render() {
 		const { name, enthusiasmLevel = 1 } = this.props;
-		const { date } = this.state;
-
-		// console.log("state", this.state)  // 这里打印出来是有date：ppp 的。
+		const { date, count } = this.state;
+		console.log('嗯，这里是 render')
 		if (enthusiasmLevel <= 0) {
 			throw new Error('You could be a little more enthusiastic. :D');
 		}
@@ -41,7 +62,7 @@ class Hello extends React.Component<IProps, IState> {
 		return (
 			<div className="hello">
 				<div className="greeting">
-					Hello {name + getExclamationMarks(enthusiasmLevel) + date + this.arr2}
+					Hello {name + getExclamationMarks(enthusiasmLevel) + date + this.arr2 + count}
 					{/* <h2>It is {date}.</h2> */}
 				</div>
 			</div>
