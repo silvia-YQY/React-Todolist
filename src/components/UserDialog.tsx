15a9b1300ad770942d7c484fbb2924f1f1eb0472
silvia-YQY <545067444@qq.com>
2018-10-13 10:40:34 +0800

    分离登录注册框选项

--- src/components/UserDialog.tsx
+++ src/components/UserDialog.tsx
@@ -14,55 +14,62 @@ export interface IState {
 
 
 export default class UserDialog extends React.Component<{}, IState>{
-	constructor(props: any){
-    super(props)
-    this.state = {
-      selected: 'signUp'
-    }
-  }
-  public switch = (e: any) => {
-    this.setState({
-      selected: e.target.value
-    })
-  }
-  public render(){
-    return (
-      <div className="UserDialog-Wrapper">
-        <div className="UserDialog">
-          <nav>
-						<input type="radio" value="signUp" onChange={this.switch} checked={this.state.selected === 'signUp'}/> 注册
-            <input type="radio" value="signIn" onChange={this.switch} checked={this.state.selected === 'signIn'}/> 登录
-          </nav>
-          <div className="panes">
-            <form className="signUp"> {/* 注册*/}
-              <div className="row">
-                <label>用户名</label> 
-                <input type="text"/>
-              </div>
-              <div className="row">
-                <label>密码</label>
-                <input type="password"/>
-              </div>
-              <div className="row actions">
-                <button type="submit">注册</button>
-              </div>
-            </form>
-            <form className="signIn"> {/* 登录*/}
-              <div className="row">
-                <label>用户名</label>
-                <input type="text"/>
-              </div>
-              <div className="row">
-                <label>密码</label>
-                <input type="password"/>
-              </div>
-              <div className="row actions">
-                <button type="submit">登录</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    )
-  }
+	constructor(props: any) {
+		super(props)
+		this.state = {
+			selected: 'signUp'
+		}
+	}
+	public switch = (e: any) => {
+		this.setState({
+			selected: e.target.value
+		})
+	}
+	public render() {
+		const signUpForm = (
+			<form className="signUp"> {/* 注册*/}
+				<div className="row">
+					<label>用户名</label>
+					<input type="text" />
+				</div>
+				<div className="row">
+					<label>密码</label>
+					<input type="password" />
+				</div>
+				<div className="row actions">
+					<button type="submit">注册</button>
+				</div>
+			</form>
+		)
+		const signInForm = (
+			<form className="signIn"> {/* 登录*/}
+				<div className="row">
+					<label>用户名</label>
+					<input type="text" />
+				</div>
+				<div className="row">
+					<label>密码</label>
+					<input type="password" />
+				</div>
+				<div className="row actions">
+					<button type="submit">登录</button>
+				</div>
+			</form>
+		)
+		return (
+
+			<div className="UserDialog-Wrapper">
+				<div className="UserDialog">
+					<nav>
+						<label><input type="radio" value="signUp" onChange={this.switch} checked={this.state.selected === 'signUp'} /> 注册</label>
+						<label><input type="radio" value="signIn" onChange={this.switch} checked={this.state.selected === 'signIn'} /> 登录</label>
+					</nav>
+					<div className="panes">						
+						{this.state.selected === 'signUp' ? signUpForm : null}
+            {this.state.selected === 'signIn' ? signInForm : null}
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
