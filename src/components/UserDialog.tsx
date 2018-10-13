9cbfad937a2bbb86b8e7df4b1ab3a41bca7314c0
silvia-YQY <545067444@qq.com>
2018-10-13 17:18:02 +0800

    添加错误信息提示

--- src/components/UserDialog.tsx
+++ src/components/UserDialog.tsx
@@ -45,14 +45,17 @@ export default class UserDialog extends React.Component<IProps, IState>{
 			this.props.onSignUp.call(null, user)
 		}
 		const errorFn = (error: any) => {
-			switch(error.code){
-        case 202:
-          alert('用户名已被占用')
-          break
-        default:
-          alert(error)
-          break
-      }
+			switch (error.code) {
+				case 201:
+					alert('没有提供密码，或者密码为空')
+					break
+				case 202:
+					alert('用户名已被占用')
+					break
+				default:
+					alert(error)
+					break
+			}
 
 		}
 		signUp(username, password, successFn, errorFn)   // leancloud 注册
@@ -66,14 +69,26 @@ export default class UserDialog extends React.Component<IProps, IState>{
 			this.props.onSignIn.call(null, user)
 		}
 		const errorFn = (error: any) => {
-			switch(error.code){
-        case 210:
-          alert('用户名与密码不匹配')
-          break
-        default:
-          alert(error)
-          break
-      }
+			switch (error.code) {
+				case 210:
+					alert('用户名与密码不匹配')
+					break
+				case 211:
+					alert('找不到用户')
+					break
+				case 217:
+					alert('无效的用户名，不允许空白用户名')
+					break
+				case 218:
+					alert('无效的密码，不允许空白密码')
+					break
+				case 219:
+					alert('失败登录超过6次，请15分钟后重新尝试')
+					break
+				default:
+					alert(error)
+					break
+			}
 		}
 		signIn(username, password, successFn, errorFn)  // leancloud 登录
 	}
@@ -81,7 +96,7 @@ export default class UserDialog extends React.Component<IProps, IState>{
 	// 改变表单内容是保存
 	public changeFormData = (key: string, e: any): void => {
 		// e.persist();
-		const stateCopy = deepClone(JSON.stringify(this.state)  // 用 JSON 深拷贝
+		const stateCopy = deepClone(this.state)  // 用 JSON 深拷贝
 		stateCopy.formData[key] = e.target.value
 		this.setState(stateCopy)
 	}
