2c2ede4fcfccd415de4038065c3fc250b717ea4c
silvia-YQY <545067444@qq.com>
2018-10-13 16:07:59 +0800

    用户进入页面时，读取上次登录的user

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ class App extends React.Component<{}, IState> {
 		this.state = {
 			newTodo: 'pleace input something',
 			todoList: [],
-			user: getCurrentUser() || {},
+			user: getCurrentUser() || {}, 
 		}		
 	}
 
