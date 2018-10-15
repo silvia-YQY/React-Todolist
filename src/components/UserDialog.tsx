245812d7a10d970516b4402cd9caf70dd3ad9719
silvia-YQY <545067444@qq.com>
2018-10-15 08:42:13 +0800

    添加忘记密码按钮

--- src/components/UserDialog.tsx
+++ src/components/UserDialog.tsx
@@ -133,6 +133,7 @@ export default class UserDialog extends React.Component<IProps, IState>{
 				</div>
 				<div className="row actions">
 					<button type="submit">登录</button>
+					<a href="javascript:;">忘记密码了？</a>
 				</div>
 			</form>
 		)
